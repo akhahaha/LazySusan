c04fe42f2b32ee37812f127c7a5f363314d26342
Shubham Joshi <shubhamjoshi@ucla.edu>
2015-11-23 21:08:50 -0800

    Patched Result because Java does not like algebraic data types. Todd Millstein weeps silently in the corner

--- app/src/main/java/com/alankhazam/lazysusan/Result.java
+++ app/src/main/java/com/alankhazam/lazysusan/Result.java
@@ -4,16 +4,19 @@ package com.alankhazam.lazysusan;
  * Created by Shubham on 11/23/2015.
  */
 public abstract class Result<T,F> {
+    public abstract boolean isOK();
     public static class OK<T, F> extends Result<T, F>{
         public final T data;
         public OK(T data){
             this.data = data;
         }
+        @Override public boolean isOK(){return true;}
     }
     public static class Error<T, F> extends Result<T, F>{
         public final F error;
         public Error(F error){
             this.error = error;
         }
+        @Override public boolean isOK(){return false;}
     }
 }
